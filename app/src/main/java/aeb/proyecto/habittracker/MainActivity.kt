b888dd5efb0c33fe047372115d8475e90648f94f
Dédalo <aayronmaiden@gmail.com>
2024-12-02 20:11:07 +0100

    TopBar y bottomBar

--- app/src/main/java/aeb/proyecto/habittracker/MainActivity.kt
+++ app/src/main/java/aeb/proyecto/habittracker/MainActivity.kt
@@ -1,16 +1,31 @@
 package aeb.proyecto.habittracker
 
+import aeb.proyecto.habittracker.ui.components.text.TitleLargeText
+import aeb.proyecto.habittracker.ui.components.text.TitleMediumText
 import aeb.proyecto.habittracker.ui.theme.HabitTrackerTheme
+import aeb.proyecto.habittracker.ui.theme.primaryColorApp
+import aeb.proyecto.habittracker.ui.theme.secondaryColorApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
@@ -28,13 +43,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AppContent(){
-    Scaffold (
-        topBar = {},
-        bottomBar = {}
-    ){ innerPadding ->
-        Column (modifier = Modifier.padding(innerPadding).fillMaxSize()){
+fun AppContent() {
+    Scaffold(
+        topBar = { TopBatHabit() },
+        bottomBar = {
+            BottomNavigationHabit()
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .background(primaryColorApp)) {
 
         }
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBatHabit(){
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = secondaryColorApp,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            TitleLargeText(text = stringResource(R.string.topbar_habit), textAlign = TextAlign.Center)
+        }
+    )
+}
+
+@Composable
+fun BottomNavigationHabit(){
+
+    NavigationBar(
+        contentColor = secondaryColorApp,
+        containerColor = secondaryColorApp
+    ) {
+        NavigationBarItem(
+            onClick = {},
+            selected = false,
+            icon = {}
+        )
+    }
+
 }
--- app/src/main/java/aeb/proyecto/habittracker/ui/theme/Color.kt
+++ app/src/main/java/aeb/proyecto/habittracker/ui/theme/Color.kt
@@ -1,6 +1,9 @@
 package aeb.proyecto.habittracker.ui.theme
 
+import android.R.id.primary
+import androidx.compose.material3.ColorScheme
 import androidx.compose.ui.graphics.Color
+
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +12,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val quartiaryColorApp = Color(0xFF625984)
+val terciaryColorApp = Color(0xFF433c68)
+val secondaryColorApp = Color(0xFF3a3053)
+val primaryColorApp = Color(0xFF18122a)
