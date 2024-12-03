4c2fa6642481dddb3520d133fba22fcfafdd42b0
Dédalo <aayronmaiden@gmail.com>
2024-12-03 02:32:45 +0100

    BottomBar con sealed Class

--- app/src/main/java/aeb/proyecto/habittracker/MainActivity.kt
+++ app/src/main/java/aeb/proyecto/habittracker/MainActivity.kt
@@ -2,18 +2,12 @@ package aeb.proyecto.habittracker
 
 import aeb.proyecto.habittracker.ui.components.text.LabelSmallText
 import aeb.proyecto.habittracker.ui.components.text.TitleLargeText
-import aeb.proyecto.habittracker.ui.components.text.TitleMediumText
-import aeb.proyecto.habittracker.ui.navigation.Achievements
-import aeb.proyecto.habittracker.ui.navigation.Habits
 import aeb.proyecto.habittracker.ui.navigation.NavigationWrapper
-import aeb.proyecto.habittracker.ui.navigation.Settings
-import aeb.proyecto.habittracker.ui.navigation.Statistics
+import aeb.proyecto.habittracker.ui.navigation.listBottomBarScreens
 import aeb.proyecto.habittracker.ui.theme.HabitTrackerTheme
 import aeb.proyecto.habittracker.ui.theme.primaryColorApp
 import aeb.proyecto.habittracker.ui.theme.secondaryColorApp
-import aeb.proyecto.habittracker.utils.Constans.NAVIGATIONBARITEMS
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -28,23 +22,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -71,10 +62,12 @@ fun AppContent(navController: NavHostController) {
             BottomNavigationHabit(navController)
         }
     ) { innerPadding ->
-        Column(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()
-            .background(primaryColorApp)) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(primaryColorApp)
+        ) {
             NavigationWrapper(navController = navController)
         }
     }
@@ -83,7 +76,7 @@ fun AppContent(navController: NavHostController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBatHabit(navController: NavHostController){
+fun TopBatHabit(navController: NavHostController) {
 
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
@@ -91,7 +84,10 @@ fun TopBatHabit(navController: NavHostController){
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
         title = {
-            TitleLargeText(text = stringResource(R.string.topbar_habit), textAlign = TextAlign.Center)
+            TitleLargeText(
+                text = stringResource(R.string.topbar_habit),
+                textAlign = TextAlign.Center
+            )
         }
     )
 }
@@ -99,8 +95,10 @@ fun TopBatHabit(navController: NavHostController){
 @Composable
 fun BottomNavigationHabit(navController: NavHostController) {
 
-    val menuItems = NAVIGATIONBARITEMS
-    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+    val menuItems = remember { listBottomBarScreens }
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
     NavigationBar(
         contentColor = secondaryColorApp,
@@ -108,18 +106,26 @@ fun BottomNavigationHabit(navController: NavHostController) {
     ) {
         menuItems.forEach {
             NavigationBarItem(
-                selected = currentDestination?.hierarchy?.any {
-                    item -> item.hasRoute(it.optionNavigation::class) } == true,
-                onClick = { navController.navigate(it.optionNavigation) },
+                selected = currentDestination?.hierarchy?.any
+                    {item -> item.route == it.route::class.qualifiedName } == true,
+                onClick = {
+                    navController.navigate(it.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
                 icon = {
                     Icon(
                         painter = painterResource(it.icon),
-                        contentDescription = stringResource(it.title),
+                        contentDescription = stringResource(it.label),
                         modifier = Modifier.size(24.dp)
                     )
                 },
                 label = {
-                    LabelSmallText(stringResource(it.title))
+                    LabelSmallText(stringResource(it.label))
                 },
                 colors = NavigationBarItemDefaults.colors(
                     selectedIconColor = Color.Black,
--- app/src/main/java/aeb/proyecto/habittracker/ui/navigation/Navigations.kt
+++ app/src/main/java/aeb/proyecto/habittracker/ui/navigation/Navigations.kt
@@ -1,5 +1,8 @@
 package aeb.proyecto.habittracker.ui.navigation
 
+import aeb.proyecto.habittracker.R
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -13,3 +16,25 @@ object Achievements
 
 @Serializable
 object Settings
+
+@Serializable
+sealed class BottomBarScreens<T>(@StringRes val label:Int, @DrawableRes val icon:Int, val route:T){
+    @Serializable
+    data object HabitsBottom:BottomBarScreens<Habits>(R.string.bottombar_habit, R.drawable.ic_calendar, Habits)
+
+    @Serializable
+    data object StatisticsBottom:BottomBarScreens<Statistics>(R.string.bottombar_stadistics, R.drawable.ic_statistics, Statistics)
+
+    @Serializable
+    data object AchievementsBottom:BottomBarScreens<Achievements>(R.string.bottombar_achievements, R.drawable.ic_achievement, Achievements)
+
+    @Serializable
+    data object SettingsBottom:BottomBarScreens<Settings>(R.string.bottombar_settins, R.drawable.ic_settings, Settings)
+}
+
+val listBottomBarScreens = listOf(
+    BottomBarScreens.HabitsBottom,
+    BottomBarScreens.StatisticsBottom,
+    BottomBarScreens.AchievementsBottom,
+    BottomBarScreens.SettingsBottom
+).map { it as BottomBarScreens<Any> }
