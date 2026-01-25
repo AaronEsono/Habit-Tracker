c53dec61acea9543be2a5e88ffe3779679f352cb
Dedalo <aaron.esono576@gmail.com>
2026-01-25 12:43:05 +0100

    StatisticsScreen.kt, calendar 3

--- feature/statistics/src/main/java/aeb/proyecto/statistics/components/common/calendar/CalendarItem.kt
+++ feature/statistics/src/main/java/aeb/proyecto/statistics/components/common/calendar/CalendarItem.kt
@@ -1,10 +1,13 @@
 package aeb.proyecto.statistics.components.common.calendar
 
 import aeb.proyecto.room.entities.relations.HabitWithDay
+import aeb.proyecto.ui.constants.getContrastColor
+import aeb.proyecto.ui.dimmens.Dimmens.spacing1
 import aeb.proyecto.ui.dimmens.Dimmens.spacing12
 import aeb.proyecto.ui.text.LabelMediumText
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,9 +19,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import java.time.LocalDate
 
-// Poner diferentes tipos de tonalidad dependiendo si el habito esta hecho al completo o no
 // Poner la cantidad exacta, si se ha hecho, en los dias correspondientes
 
 @Composable
@@ -33,31 +36,40 @@ fun CalendarItem(
         habitWithDay?.day?.goalDone?.toFloat() ?: 0f
     }
 
-    // Arreglar esto, el alpha no se aplica
     val colorHabit = remember(habitWithDay) {
         val habit = habitWithDay?.habit
         val goal = habit?.goal?.toFloat() ?: 0f
         val baseColor = Color(habit?.color ?: 0)
 
-        if (goal >= goalDone) baseColor else baseColor.copy(alpha = 0.5f)
+
+        if (goalDone < goal)
+            baseColor.copy(alpha = 0.5f)
+        else
+            baseColor
     }
 
     val isInMonth = remember (day,monthSelected){
         day.month == monthSelected.month
     }
 
-    val color = if (goalDone <= 0) MaterialTheme.colorScheme.background else colorHabit
+    val dayWithGoal = remember (habitWithDay){
+        goalDone > 0
+    }
+
+    val color = if (goalDone <= 0) MaterialTheme.colorScheme.primary else colorHabit
 
     Box(
         modifier = modifier
             .alpha(if (isInMonth) 1f else 0.20f)
             .aspectRatio(1f)
+            .border(if (dayWithGoal) 2.dp else 0.dp, color, RoundedCornerShape(spacing12))
             .clip(RoundedCornerShape(spacing12))
             .background(color),
         contentAlignment = Alignment.Center
     ){
         LabelMediumText(
-            day.dayOfMonth.toString()
+            day.dayOfMonth.toString(),
+            color = getContrastColor(color)
         )
     }
 
