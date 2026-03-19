58d3510d53dde01fa834902dfe1a763588cb87f5
Dedalo <aaron.esono576@gmail.com>
2026-03-19 14:30:30 +0100

    StatisticsScreen.kt, cambio de versiones por actualizacion de android Studio

--- feature/statistics/src/main/java/aeb/proyecto/statistics/components/common/graphics/monthGraphics/MonthGraphics.kt
+++ feature/statistics/src/main/java/aeb/proyecto/statistics/components/common/graphics/monthGraphics/MonthGraphics.kt
@@ -2,11 +2,9 @@ package aeb.proyecto.statistics.components.common.graphics.monthGraphics
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import com.patrykandpatrick.vico.compose.cartesian.data.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
 
 @Composable
 fun MonthGraphics(){
-
     val modelProducer = remember{ CartesianChartModelProducer() }
-
 }
